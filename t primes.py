1d43e261265786e792e993f8db41d8361cc10cc4
rifat <rifatalmamun.cse@gmail.com>
2021-07-03 01:28:39 +0600

    solution t primes

--- t primes.py
+++ t primes.py
@@ -1,10 +1,8 @@
 
 def solution(n):
     lst = []
-    for i in range(1,n+1,1):
-        lst_input = int(input())
-        lst.append(lst_input)
-    
+    lst = list(map(int, input().split()[:n]))
+
     for i in lst:
             flag = 0
             for j in range(2,i,1):
